d185672e49b9c267befb40a2ca1fe78d44a93575
KB Lee <klee869@gatech.edu>
2022-04-04 22:45:02 -0400

    Mediapipe object works

--- src/App.js
+++ src/App.js
@@ -1,16 +1,75 @@
 import { useRef, useState, useEffect } from "react";
 import "./App.css";
-import { Hands } from "@mediapipe/hands";
+import * as Hands from "@mediapipe/hands";
 import MainScreen from "./components/MainScreen";
 import VideoRecorder from "react-video-recorder";
-import { Camera } from "@mediapipe/camera_utils";
+import * as Camera from "@mediapipe/camera_utils";
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
 import Webcam from "react-webcam";
 
 export default function App() {
-  const hands = new Hands();
+  const handRef = useRef(null);
   const webcamRef = useRef(null);
-  console.log(hands);
+  const canvasRef = useRef(null);
+
+  function onResults(results) {
+    const videoWidth = webcamRef.current.video.videoWidth;
+    const videoHeight = webcamRef.current.video.videoHeight;
+
+    canvasRef.current.width = videoWidth;
+    canvasRef.current.height = videoHeight;
+    const canvasElement = canvasRef.current;
+    const canvasCtx = canvasElement.getContext("2d");
+
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+    if (results.multiHandLandmarks) {
+      for (const landmarks of results.multiHandLandmarks) {
+        drawConnectors(canvasCtx, landmarks, Hands.HAND_CONNECTIONS, {
+          color: "#00FF00",
+          lineWidth: 5,
+        });
+        drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: 2 });
+      }
+    }
+    canvasCtx.restore();
+  }
+
   useEffect(() => {
+    const hands = new Hands.Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
+    });
+    handRef.current = hands;
+    hands.setOptions({
+      maxNumHands: 2,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+    hands.onResults(onResults);
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      webcamRef.current.crossOrigin = "anonymous";
+      const camera = new Camera.Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          await hands.send({ image: webcamRef.current.video });
+        },
+        width: 1280,
+        height: 720,
+      });
+      camera.start();
+    }
+
     navigator.getUserMedia(
       { audio: true, video: true },
       (stream) => {
@@ -19,11 +78,13 @@ export default function App() {
       (err) => console.log(err)
     );
   }, []);
+
   return (
     <div className="App">
       React Webcam
-      <Webcam ref={webcamRef} />
-      <canvas />
+      <Webcam ref={webcamRef} hidden />
+      {/* <video className="input_video" ref={webcamRef} /> */}
+      <canvas ref={canvasRef} />
     </div>
   );
 }
